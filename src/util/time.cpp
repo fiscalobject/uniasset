8e24c73bbe09cf8f6629f4b0642fdeeb779d54f4
S <ramires9@pm.me>
2021-05-02 12:32:15 +0300

    Reverted ifdef changes

--- src/util/time.cpp
+++ src/util/time.cpp
@@ -79,15 +79,10 @@ void MilliSleep(int64_t n)
 std::string FormatISO8601DateTime(int64_t nTime) {
     struct tm ts;
     time_t time_val = nTime;
-//#ifdef _MSC_VER 
-//    gmtime_s(&ts, &time_val);
-//#else
-//    gmtime_r(&time_val, &ts);
-//#endif
-#ifdef gmtime_r
-    gmtime_r(&time_val, &ts);
-#else
+#ifdef _MSC_VER
     gmtime_s(&ts, &time_val);
+#else
+    gmtime_r(&time_val, &ts);
 #endif
     return strprintf("%04i-%02i-%02iT%02i:%02i:%02iZ", ts.tm_year + 1900, ts.tm_mon + 1, ts.tm_mday, ts.tm_hour, ts.tm_min, ts.tm_sec);
 }
@@ -95,15 +90,10 @@ std::string FormatISO8601DateTime(int64_t nTime) {
 std::string FormatISO8601Date(int64_t nTime) {
     struct tm ts;
     time_t time_val = nTime;
-//#ifdef _MSC_VER 
-//    gmtime_s(&ts, &time_val);
-//#else
-//    gmtime_r(&time_val, &ts);
-//#endif
-#ifdef gmtime_r
-    gmtime_r(&time_val, &ts);
-#else
+#ifdef _MSC_VER
     gmtime_s(&ts, &time_val);
+#else
+    gmtime_r(&time_val, &ts);
 #endif
     return strprintf("%04i-%02i-%02i", ts.tm_year + 1900, ts.tm_mon + 1, ts.tm_mday);
 }
@@ -111,15 +101,10 @@ std::string FormatISO8601Date(int64_t nTime) {
 std::string FormatISO8601Time(int64_t nTime) {
     struct tm ts;
     time_t time_val = nTime;
-//#ifdef _MSC_VER 
-//    gmtime_s(&ts, &time_val);
-//#else
-//    gmtime_r(&time_val, &ts);
-//#endif
-#ifdef gmtime_r
-    gmtime_r(&time_val, &ts);
-#else
+#ifdef _MSC_VER
     gmtime_s(&ts, &time_val);
+#else
+   gmtime_r(&time_val, &ts);
 #endif
     return strprintf("%02i:%02i:%02iZ", ts.tm_hour, ts.tm_min, ts.tm_sec);
 }
